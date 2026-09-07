28dcb5bebea187be2909bf4df149459efdbe6c31
fadlee <fad.lee@hotmail.com>
2026-09-07 08:00:30 +0700

    fix(registry): avoid duplicate provider import identifier

--- open-sse/providers/registry/index.js
+++ open-sse/providers/registry/index.js
@@ -123,7 +123,7 @@ import p118 from "./selfhosted-tts.js";
 import p119 from "./selfhosted-embedding.js";
 import p120 from "./fish-audio.js";
 import p121 from "./alitp-intl.js";
-import p122 from "./xquik.js";
+import p124 from "./xquik.js";
 
 export default [
   p0,
@@ -248,5 +248,5 @@ export default [
   p119,
   p120,
   p121,
-  p122,
+  p124,
 ];
